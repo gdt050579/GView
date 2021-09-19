763ebe1ff7b11377a6cb548b3ec3637a561259dc
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-09-19 08:51:37 +0300

    Prepare Menus

--- GView/include/GViewApp.hpp
+++ GView/include/GViewApp.hpp
@@ -5,9 +5,20 @@ namespace GView
 {
     namespace App
     {
+        namespace MenuCommands
+        {
+            constexpr int ARRANGE_VERTICALLY = 100000;
+            constexpr int ARRANGE_HORIZONTALLY = 100001;
+            constexpr int ARRANGE_CASCADE = 100002;
+            constexpr int ARRANGE_GRID = 100003;
+        };
         class Instance
         {
+            AppCUI::Controls::Menu* mnuWindow;
             std::vector<GView::Type::Plugin> typePlugins;
+
+            bool BuildMainMenus();
+            bool LoadSettings();
         public:
             bool Init();
             void Run();
--- GView/src/Instance.cpp
+++ GView/src/Instance.cpp
@@ -2,14 +2,58 @@
 
 using namespace GView::App;
 using namespace AppCUI::Application;
+using namespace AppCUI::Controls;
+using namespace AppCUI::Input;
+
+struct _MenuCommand_
+{
+    std::string_view name;
+    int commandID;
+    Key shortCutKey;
+};
+constexpr _MenuCommand_ menuArrange[] = {
+    {"Arrange &Vertically", MenuCommands::ARRANGE_VERTICALLY, Key::None},
+    {"Arrange &Horizontally", MenuCommands::ARRANGE_HORIZONTALLY, Key::None},
+    {"&Cascade mode", MenuCommands::ARRANGE_CASCADE, Key::None},
+    {"&Grid", MenuCommands::ARRANGE_GRID, Key::None},
+};
+
+bool AddMenuCommands(Menu* mnu, const _MenuCommand_* list, size_t count, bool addSeperatorAfterList)
+{
+    while (count > 0)
+    {
+        CHECK(mnu->AddCommandItem(list->name, list->commandID, list->shortCutKey) != InvalidItemHandle, false, "Fail to add %s to menu !", list->name.data());
+        count--;
+        list++;
+    }
+    if (addSeperatorAfterList)
+    {
+        CHECK(mnu->AddSeparator() != InvalidItemHandle, false, "Fail to add separator !");
+    }
+    return true;
+}
+
+bool Instance::LoadSettings()
+{
+    // process all settings and set up plugins
+    return true;
+}
+bool Instance::BuildMainMenus()
+{
+    CHECK(mnuWindow = AppCUI::Application::AddMenu("&Windows"), false, "Unable to create `Windows` menu");
+    CHECK(AddMenuCommands(mnuWindow, menuArrange, 4, false), false, "");
+    return true;
+}
 
 bool Instance::Init()
 {
     InitializationData initData;
     initData.Flags = InitializationFlags::Menu | InitializationFlags::CommandBar;
     CHECK(AppCUI::Application::Init(initData), false, "Fail to initialize AppCUI framework !");
-
-
+    // reserve some space fo type
+    this->typePlugins.reserve(128);
+    CHECK(LoadSettings(), false, "Fail to load settings !");
+    CHECK(BuildMainMenus(), false, "Fail to create bundle menus !");
     return true;
 }
 void Instance::Run()
