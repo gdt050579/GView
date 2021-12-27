a72aeff7bb3385dcec7162af5c11b7baf620f483
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2021-12-27 21:54:45 +0200

    Selection editor - add settings (2)

--- GViewCore/src/View/BufferViewer/SelectionEditor.cpp
+++ GViewCore/src/View/BufferViewer/SelectionEditor.cpp
@@ -11,18 +11,26 @@ constexpr int32 BTN_ID_CANCEL = 4;
 SelectionEditor::SelectionEditor(Reference<Utils::Selection> _selection, uint32 index, Reference<SettingsData> _settings)
     : Window("Selection Editor", "d:c,w:61,h:10", WindowFlags::None), selection(_selection), zoneIndex(index), settings(_settings)
 {
-    Factory::Label::Create(this, "&Offset", "x:1,y:1,w:10");
-    Factory::Label::Create(this, "&Type", "x:35,y:1,w:10");
-    Factory::Label::Create(this, "&Size", "x:1,y:3,w:10");
-    Factory::Label::Create(this, "&Base", "x:35,y:3,w:10");
-    txOffset  = Factory::TextField::Create(this, "", "x:11,y:1,w:20");
-    cbOfsType = Factory::ComboBox::Create(this, "x:40,y:1,w:17");
-    txSize    = Factory::TextField::Create(this, "", "x:11,y:3,w:20");
-    cbBase    = Factory::ComboBox::Create(this, "x:40,y:3,w:17", "Auto,Dec,Hex");
+    LocalString<128> tmp;
+    for (uint32 tr=0;tr<settings->translationMethodsCount;tr++)
+    {
+        if (tr > 0)
+            tmp.AddChar(',');
+        tmp.Add(settings->translationMethods[tr].name);
+    }
+    Factory::Label::Create(this, "&Offset", "x:1,y:1,w:6");
+    Factory::Label::Create(this, "&Type", "x:30,y:1,w:5");
+    Factory::Label::Create(this, "&Size", "x:1,y:3,w:5");
+    Factory::Label::Create(this, "&Base", "x:30,y:3,w:5");
+    txOffset  = Factory::TextField::Create(this, "", "x:8,y:1,w:18");
+    cbOfsType = Factory::ComboBox::Create(this, "x:35,y:1,w:22",tmp.ToStringView());
+    txSize    = Factory::TextField::Create(this, "", "x:8,y:3,w:18");
+    cbBase    = Factory::ComboBox::Create(this, "x:35,y:3,w:22", "Auto,Dec,Hex");
     Factory::Button::Create(this, "OK", "l:2,b:0,w:13", BTN_ID_OK);
     Factory::Button::Create(this, "ClearSel", "l:16,b:0,w:13", BTN_ID_CLEAR);
     Factory::Button::Create(this, "Reload", "l:30,b:0,w:13", BTN_ID_RELOAD);
     Factory::Button::Create(this, "Cancel", "l:44,b:0,w:13", BTN_ID_CANCEL);
+
 
     RefreshSizeAndOffset();
 }
