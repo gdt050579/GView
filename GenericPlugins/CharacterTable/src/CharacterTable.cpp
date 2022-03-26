f60779562bcf9f2b9d618d40f083e2f7be122112
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-03-26 12:07:14 +0200

    Character table generic plugin example

--- GenericPlugins/CharacterTable/src/CharacterTable.cpp
+++ GenericPlugins/CharacterTable/src/CharacterTable.cpp
@@ -8,14 +8,33 @@ using namespace GView::Utils;
 using namespace GView;
 using namespace GView::View;
 
+constexpr int CMD_BUTTON_CLOSE = 1;
+class CharacterTableExample : public Window, public Handlers::OnButtonPressedInterface
+{
+  public:
+    CharacterTableExample() : Window("Character Table", "d:c,w:70,h:20", WindowFlags::Sizeable | WindowFlags::Maximized)
+    {
+        Factory::CharacterTable::Create(this, "l:1,t:1,r:1,b:3");
+        Factory::Button::Create(this, "&Close", "d:b,w:20", CMD_BUTTON_CLOSE)->Handlers()->OnButtonPressed = this;
+    }
+    void OnButtonPressed(Reference<Button>) override
+    {
+        this->Exit(0);
+    }
+};
+
 extern "C"
 {
     PLUGIN_EXPORT bool Run(const string_view command)
     {
         // all good
-
-        AppCUI::Dialogs::MessageBox::ShowNotification("Run", command);
-        return true;
+        if (command == "CharacterTable")
+        {
+            CharacterTableExample dlg;
+            dlg.Show();
+            return true;
+        }
+        return false;
     }
     PLUGIN_EXPORT void UpdateSettings(IniSection sect)
     {
