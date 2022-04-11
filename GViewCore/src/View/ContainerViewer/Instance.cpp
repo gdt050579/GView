5b8718b3fa4cf9d1aebaa8bac0877b7a5388e63c
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-04-11 19:45:01 +0300

    Update to latest AppCUI

--- GViewCore/src/View/ContainerViewer/Instance.cpp
+++ GViewCore/src/View/ContainerViewer/Instance.cpp
@@ -41,6 +41,8 @@ Instance::Instance(const std::string_view& _name, Reference<GView::Object> _obj,
           { { "Field", TextAlignament::Left, 20 }, { "Value", TextAlignament::Left, 200 } },
           ListViewFlags::HideColumns);
 
+    this->items = Factory::TreeView::Create(this, "l:0,t:10,r:0,b:0");
+    
     //this->items = Factory::Tree::Create(this, "l:0,t:10,r:0,b:0", TreeFlags::None, settings->columnsCount);
     //for (uint32 idx = 0; idx < settings->columnsCount; idx++)
     //{
