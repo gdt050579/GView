c7472ff316cee054357918d2202b32387f1e3569
Gheorghita MUTU <gheorghitamutu@gmail.com>
2022-05-20 15:39:51 +0300

    + workflow - windows OpenSSL root dir
    Signed-off-by: Gheorghita MUTU <gheorghitamutu@gmail.com>

--- .github/workflows/win_openssl.py
+++ .github/workflows/win_openssl.py
@@ -6,14 +6,14 @@ from io import BytesIO  # Python 3
 import zipfile
 
 OPENSSL_URL = os.getenv('OPENSSL_URL')
-OPENSSL_PATH = os.getenv('OPENSSL_PATH')
+OPENSSL_ROOT_DIR = os.getenv('OPENSSL_ROOT_DIR')
 
-if not os.path.exists(OPENSSL_PATH):
-    os.makedirs(OPENSSL_PATH, exist_ok=True)
+if not os.path.exists(OPENSSL_ROOT_DIR):
+    os.makedirs(OPENSSL_ROOT_DIR, exist_ok=True)
     
 def get_and_extract(url: str) -> None:
     zip_response = requests.get(url, stream=True)
     zip = zipfile.ZipFile(BytesIO(zip_response.content))
-    zip.extractall(OPENSSL_PATH)
+    zip.extractall(OPENSSL_ROOT_DIR)
 
 get_and_extract(OPENSSL_URL)
