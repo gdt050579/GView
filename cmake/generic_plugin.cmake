b03b1fe3fff3428b91e87737f89e1d9b7f60c10b
Gheorghita MUTU <gheorghitamutu@gmail.com>
2022-07-03 18:24:07 +0300

    [Build] # fix build directory structure artefacts generation for OSX & Unix
    Signed-off-by: Gheorghita MUTU <gheorghitamutu@gmail.com>

--- cmake/generic_plugin.cmake
+++ cmake/generic_plugin.cmake
@@ -30,8 +30,8 @@ function (create_generic_plugin generic_plugin_name)
 	file(GLOB_RECURSE PROJECT_HEADERS include/*.hpp)
 	target_sources(${PROJECT_NAME} PRIVATE ${PROJECT_HEADERS})
 
-    include(CheckIPOSupported)
-    check_ipo_supported(RESULT supported OUTPUT error)
+        include(CheckIPOSupported)
+        check_ipo_supported(RESULT supported OUTPUT error)
 
 	if( supported )
 		message(STATUS "${PROJECT_NAME} => IPO / LTO enabled")
@@ -49,16 +49,22 @@ function (create_generic_plugin generic_plugin_name)
 	set_target_properties(${PROJECT_NAME} PROPERTIES SUFFIX ".gpl")
 	
 	set_target_properties(${PROJECT_NAME} PROPERTIES
-	               FOLDER "GenericPlugins"
-                   RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}/GenericPlugins"
-                   RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/GenericPlugins")
-	
+	            FOLDER "GenericPlugins"
+                RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}/GenericPlugins"
+                RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE}/GenericPlugins"
+		        ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_ARCHIVE_OUTPUT_DIRECTORY}/GenericPlugins"
+			    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/GenericPlugins")
+   	   
 	get_target_property(F ${PROJECT_NAME} FOLDER)
 	get_target_property(RODD ${PROJECT_NAME} RUNTIME_OUTPUT_DIRECTORY_DEBUG)
 	get_target_property(RODR ${PROJECT_NAME} RUNTIME_OUTPUT_DIRECTORY_RELEASE)
+	get_target_property(AOD ${PROJECT_NAME} ARCHIVE_OUTPUT_DIRECTORY)
+	get_target_property(LOD ${PROJECT_NAME} LIBRARY_OUTPUT_DIRECTORY)
 	
 	message(STATUS "${PROJECT_NAME} => FOLDER = ${F}")
 	message(STATUS "${PROJECT_NAME} => RUNTIME_OUTPUT_DIRECTORY_DEBUG = ${RODD}")
 	message(STATUS "${PROJECT_NAME} => RUNTIME_OUTPUT_DIRECTORY_RELEASE = ${RODR}")
+	message(STATUS "${PROJECT_NAME} => ARCHIVE_OUTPUT_DIRECTORY = ${AOD}")
+	message(STATUS "${PROJECT_NAME} => LIBRARY_OUTPUT_DIRECTORY = ${LOD}")
 endfunction()
 
