6945e5379228a817b95bf91bbdf33a8fb3d3154f
Gheorghita Mutu <gheorghitamutu@gmail.com>
2022-08-25 16:43:57 +0300

    Update buildMac.sh

--- buildMac.sh
+++ buildMac.sh
@@ -6,7 +6,7 @@ brew install --build-from-source --verbose --debug openssl@1.1
 OPENSSL_ROOT_DIR=$(brew --prefix openssl@1.1)
 BUILD_TYPE="Debug"
 
-cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}} -DOPENSSL_ROOT_DIR="$OPENSSL_ROOT_DIR" -DOPENSSL_LIBRARIES="$OPENSSL_ROOT_DIR/lib"
+cmake -B ./build -DCMAKE_BUILD_TYPE="$BUILD_TYPE" -DOPENSSL_ROOT_DIR="$OPENSSL_ROOT_DIR" -DOPENSSL_LIBRARIES="$OPENSSL_ROOT_DIR/lib"
 cmake --build "$BUILD_TYPE/build" --config "$BUILD_TYPE"
 
 cmake -Bbuild -G Xcode .
