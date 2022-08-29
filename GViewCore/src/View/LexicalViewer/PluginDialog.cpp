5b6c9d33fbe817105e0e3a27df5355387711c66a
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2022-08-29 11:31:06 +0300

    Populate plugins

--- GViewCore/src/View/LexicalViewer/PluginDialog.cpp
+++ GViewCore/src/View/LexicalViewer/PluginDialog.cpp
@@ -10,10 +10,16 @@ constexpr int32 BTN_ID_CANCEL  = 2;
 constexpr int32 APPLY_GROUP_ID = 1;
 
 PluginDialog::PluginDialog(PluginData& data, Reference<SettingsData> settings)
-    : Window("Plugins", "d:c,w:70,h:12", WindowFlags::ProcessReturn), pluginData(data)
+    : Window("Plugins", "d:c,w:70,h:24", WindowFlags::ProcessReturn), pluginData(data)
 {
-    this->lstPlugins        = Factory::ListView::Create(this, "l:1,t:1,r:1,b:4", { "w:200,a:l,n:Name" }, ListViewFlags::HideColumns);
-    this->cbOpenInNewWindow = Factory::CheckBox::Create(this, "Open result in &new window", "l:1,b:2,w:60");
+    this->lstPlugins        = Factory::ListView::Create(this, "l:1,t:1,r:1,b:4", { "w:25,a:l,n:Name", "w:200,a:l,n:Descrition" });
+    this->cbOpenInNewWindow = Factory::CheckBox::Create(this, "Open result in &new window", "l:1,b:3,w:60");
+
+    // populate
+    for (auto& p: settings->plugins)
+    {
+        this->lstPlugins->AddItem({ p->GetName(), p->GetDescription() });
+    }
 
     // buttons
     Factory::Button::Create(this, "&Delete", "l:21,b:0,w:13", BTN_ID_OK);
