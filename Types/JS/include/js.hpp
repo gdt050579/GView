a1672433e10a31c514b90a191f18005d965ace3b
UnexomWid <uw@exom.dev>
2022-10-09 12:02:47 +0300

    add: remove comments plugin

--- Types/JS/include/js.hpp
+++ Types/JS/include/js.hpp
@@ -269,6 +269,15 @@ namespace Type
                 virtual bool CanBeAppliedOn(const GView::View::LexicalViewer::PluginData& data) override;
                 virtual GView::View::LexicalViewer::PluginAfterActionRequest Execute(GView::View::LexicalViewer::PluginData& data) override;
             };
+
+            class RemoveComments : public GView::View::LexicalViewer::Plugin
+            {
+              public:
+                virtual std::string_view GetName() override;
+                virtual std::string_view GetDescription() override;
+                virtual bool CanBeAppliedOn(const GView::View::LexicalViewer::PluginData& data) override;
+                virtual GView::View::LexicalViewer::PluginAfterActionRequest Execute(GView::View::LexicalViewer::PluginData& data) override;
+            };
         } // namespace Plugins
 
         class JSFile : public TypeInterface, public GView::View::LexicalViewer::ParseInterface
@@ -308,6 +317,7 @@ namespace Type
             {
                 Plugins::AddStrings addStrings;
                 Plugins::ReverseStrings reverseStrings;
+                Plugins::RemoveComments removeComments;
             } plugins;
             JSFile();
             virtual ~JSFile()
--- Types/JS/src/js.cpp
+++ Types/JS/src/js.cpp
@@ -32,6 +32,7 @@ extern "C"
 
         settings.AddPlugin(&js->plugins.addStrings);
         settings.AddPlugin(&js->plugins.reverseStrings);
+        settings.AddPlugin(&js->plugins.removeComments);
         win->CreateViewer("Lexical", settings);
 
         win->CreateViewer<TextViewer::Settings>("Text View");
