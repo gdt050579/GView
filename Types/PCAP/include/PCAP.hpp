e2b1254db34c0e8210d0179597a0b7d2a38e16b0
Raul Zaharia <raul_zaharia2006@yahoo.com>
2023-05-21 18:16:27 +0300

    imrpoved pcap to be more generic

--- Types/PCAP/include/PCAP.hpp
+++ Types/PCAP/include/PCAP.hpp
@@ -181,7 +181,7 @@ namespace Panels
             void Add_ICMPHeader(const ICMPHeader_Base* icmpBase, uint32 icmpSize);
             void Add_DNSHeader_Question(const DNSHeader_Question& question);
             void Add_TCPHeader(const TCPHeader* tcp, uint32 packetInclLen);
-            void Add_TCPHeader_Options(const TCPHeader* tcp, uint32 packetInclLen);
+            void Add_TCPHeader_Options(const uint8* optionsPtr, uint32 optionsLen);
 
           public:
             PacketDialog(
