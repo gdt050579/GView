20aa736e1cdcd8d5c470b421e0cb4b407d5e949b
Mihai Bojescu <bojescu.mihai@gmail.com>
2025-01-23 01:37:57 +0200

    feat(online-analytics): Add title, size, severity to response ui

--- GenericPlugins/OnlineAnalytics/include/ui/OnlineAnalyticsResultsUI.hpp
+++ GenericPlugins/OnlineAnalytics/include/ui/OnlineAnalyticsResultsUI.hpp
@@ -2,6 +2,8 @@
 
 #include "GView.hpp"
 
+#include "utils/Report.hpp"
+
 namespace GView::GenericPlugins::OnlineAnalytics::UI
 {
 using namespace AppCUI::Controls;
@@ -9,10 +11,17 @@ using namespace AppCUI::Controls;
 class OnlineAnalyticsResultsUI : public Window
 {
   private:
+    bool didInit;
     Reference<GView::Object> object;
+    Reference<Utils::Report> report;
+
+    Reference<Controls::Tab> tabs;
 
   public:
-    OnlineAnalyticsResultsUI(Reference<GView::Object> object);
+    OnlineAnalyticsResultsUI(Reference<GView::Object> object, Reference<Utils::Report> report);
+    bool Init();
+
+    AppCUI::Dialogs::Result Show();
 };
 
 }; // namespace GView::GenericPlugins::OnlineAnalytics::UI
--- GenericPlugins/OnlineAnalytics/src/ui/OnlineAnalyticsResultsUI.cpp
+++ GenericPlugins/OnlineAnalytics/src/ui/OnlineAnalyticsResultsUI.cpp
@@ -1,13 +1,51 @@
+#include <format>
 #include "ui/OnlineAnalyticsResultsUI.hpp"
-#include "stdio.h"
 
 namespace GView::GenericPlugins::OnlineAnalytics::UI
 {
 using namespace AppCUI::Controls;
 
-OnlineAnalyticsResultsUI::OnlineAnalyticsResultsUI(Reference<GView::Object> object) : Window("Online analytics", "d:c,w:30,h:30", WindowFlags::FixedPosition)
-{
-    this->object = object;
+constexpr int32 TAB_ID_GENERAL      = 0;
+constexpr int32 TAB_ID_HASHES       = 1;
+constexpr int32 TAB_ID_CAPABILITIES = 2;
+constexpr int32 TAB_ID_ANALYSIS     = 3;
+
+constexpr std::string SEVERITY_LABELS[] = {
+    std::string("None"), std::string("Low"), std::string("Medium"), std::string("High"), std::string("Critical"),
 };
+
+OnlineAnalyticsResultsUI::OnlineAnalyticsResultsUI(Reference<GView::Object> object, Reference<Utils::Report> report)
+    : Window("Online analytics: results", "d:c,w:80,h:24", WindowFlags::None)
+{
+    this->object  = object;
+    this->report  = report;
+    this->didInit = false;
+
+    this->tabs                                       = Factory::Tab::Create(this, "l:0,t:0,r:0,b:0", TabFlags::TopTabs | TabFlags::TabsBar);
+    Reference<Controls::TabPage> generalTabPage      = Factory::TabPage::Create(this->tabs, "General", TAB_ID_GENERAL);
+    Reference<Controls::TabPage> hashesTabPage       = Factory::TabPage::Create(this->tabs, "Hashes", TAB_ID_HASHES);
+    Reference<Controls::TabPage> capabilitiesTabPage = Factory::TabPage::Create(this->tabs, "Capabilities", TAB_ID_CAPABILITIES);
+    Reference<Controls::TabPage> analysisTabPage     = Factory::TabPage::Create(this->tabs, "Analysis", TAB_ID_ANALYSIS);
+
+    Reference<Controls::Label> nameLabel = Factory::Label::Create(generalTabPage, std::format("File name: {}", this->report->fileName), "a:t,l:1,r:1,y:1,h:1");
+    Reference<Controls::Label> sizeLabel =
+          Factory::Label::Create(generalTabPage, std::format("File size: {} B", this->report->fileSize), "a:t,l:1,r:1,y:2,h:1");
+    Reference<Controls::Label> severityLabel =
+          Factory::Label::Create(generalTabPage, std::format("Severity from provider: {}", SEVERITY_LABELS[this->report->severity]), "a:t,l:1,r:1,y:4,h:1");
+};
+
+bool OnlineAnalyticsResultsUI::Init()
+{
+    CHECK(this->didInit == false, false, "Already called init on Results UI");
+
+    this->didInit = true;
+    return true;
+}
+
+AppCUI::Dialogs::Result OnlineAnalyticsResultsUI::Show()
+{
+    CHECK(this->didInit == true, AppCUI::Dialogs::Result::Cancel, "Did not call init on Results UI");
+    return Window::Show();
+}
 
 }; // namespace GView::GenericPlugins::OnlineAnalytics::UI
