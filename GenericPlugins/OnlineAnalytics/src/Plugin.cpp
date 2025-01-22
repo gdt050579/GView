1645364523cf1678c0ab1fc45bd8f254d3086dce
Mihai Bojescu <bojescu.mihai@gmail.com>
2025-01-22 18:47:56 +0200

    feat(online-analytics): Cleanup plugin main entrypoint

--- GenericPlugins/OnlineAnalytics/src/Plugin.cpp
+++ GenericPlugins/OnlineAnalytics/src/Plugin.cpp
@@ -1,8 +1,11 @@
-#include "GView.hpp"
-#include <curl/curl.h>
 #include <iostream>
+#include <curl/curl.h>
+#include <format>
+#include "GView.hpp"
 
 #include "utils/Hashing.hpp"
+#include "utils/Report.hpp"
+#include "providers/VirusTotal.hpp"
 #include "ui/OnlineAnalyticsInitialUI.hpp"
 #include "ui/OnlineAnalyticsResultsUI.hpp"
 
@@ -10,14 +13,7 @@ namespace GView::GenericPlugins::OnlineAnalytics
 {
 constexpr std::string_view CMD_ONLINE_ANALYTICS = "OnlineAnalytics";
 
-
 extern "C" {
-
-static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
-    ((std::string*) userp)->append((char*) contents, size * nmemb);
-    return size * nmemb;
-}
 
 PLUGIN_EXPORT bool Run(const string_view command, Reference<GView::Object> object)
 {
@@ -26,35 +22,21 @@ PLUGIN_EXPORT bool Run(const string_view command, Reference<GView::Object> objec
     }
 
     UI::OnlineAnalyticsInitialUI initialUi(object);
+    CHECK(initialUi.Init() == true, false, "Could not initialise OnlineAnalytics UI");
 
     if (initialUi.Show() != AppCUI::Dialogs::Result::Ok || object->GetData().GetSize() == 0) {
         return true;
     }
 
-    String hash = Utils::HashSHA256(object);
+    Reference<Providers::IProvider> provider = initialUi.GetProvider();
+    CHECK(provider != NULL, false, "There was an error when selecting the provider: Provider is NULL");
 
-    CHECK(hash.Len() != 0, false, "There was an error when computing the hash of the file");
+    Reference<std::array<uint8, 32>> hash = Utils::HashSHA256(object);
+    CHECK(hash != NULL, false, "There was an error when computing the hash of the file");
 
-    Dialogs::MessageBox::ShowNotification("Hash", hash);
+    Reference<Utils::Report> report = provider->GetReport(hash);
+    CHECK(report != NULL, false, "There was an error when retrieving the report");
 
-    CURL* curl;
-    CURLcode res;
-    std::string readBuffer;
-    struct curl_slist* headers = NULL;
-
-    curl = curl_easy_init();
-    if (curl) {
-        headers = curl_slist_append(headers, "Accept: application/json");
-
-        curl_easy_setopt(curl, CURLOPT_URL, "https://api64.ipify.org/");
-        curl_easy_setopt(curl, CURLOPT_HEADER, headers);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        curl_easy_perform(curl);
-        curl_slist_free_all(headers);
-        curl_easy_cleanup(curl);
-        std::cout << readBuffer << std::endl;
-    }
 
     return true;
 }
