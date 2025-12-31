41bed8ead91aeacd1461231312a8381097c26780
Cristian FIODOROV <cristifv59@gmail.com>
2025-12-31 20:27:16 +0200

    yara wrapper: added CHECKs instead of ifs

--- GViewCore/src/Yara/yara_wrapper.cpp
+++ GViewCore/src/Yara/yara_wrapper.cpp
@@ -31,21 +31,19 @@ namespace GView::Yara
 {
     bool YaraScanner::Init() {
         yr_initialize();
-        if (!CreateCompiler()) {
-            return false;
-        }
+        CHECK(CreateCompiler(), false, "Failed to create YARA compiler");
         LoadRules("rules.yara");
         return true;
     }
 
     YaraScanner::~YaraScanner() {
-        if (rules) {
+        if (rules != nullptr) {
             SaveRules("rules.yara");
             yr_rules_destroy(static_cast<YR_RULES*>(rules));
             rules = nullptr;
         }
 
-        if (compiler) {
+        if (compiler != nullptr) {
             yr_compiler_destroy(static_cast<YR_COMPILER*>(compiler));
             compiler = nullptr;
         }
@@ -54,24 +52,16 @@ namespace GView::Yara
     }
 
     bool YaraScanner::CreateCompiler() {
-        YR_COMPILER* comp;
-        int result = yr_compiler_create(&comp);
-        if (result == ERROR_SUCCESS) {
-            compiler = comp;
-            return true;
-        }
-        return false;
+        YR_COMPILER** ptr_yr_compiler = reinterpret_cast<YR_COMPILER**>(&compiler);
+        CHECK(yr_compiler_create(ptr_yr_compiler) == ERROR_SUCCESS, false, "Failed to create YARA compiler");
+        return true;
     }
 
     bool YaraScanner::AddRules(const std::string& filePath) {
-        if (!compiler) {
-            return false;
-        }
+        CHECK(compiler, false, "Compiler not created");
         
         FILE* file = fopen(filePath.c_str(), "r");
-        if (!file) {
-            return false;
-        }
+        CHECK(file, false, "Failed to open file: %s", filePath.c_str());
         
         int result = yr_compiler_add_file(static_cast<YR_COMPILER*>(compiler), file, nullptr, filePath.c_str());
         fclose(file);
@@ -80,64 +70,51 @@ namespace GView::Yara
     }
 
     bool YaraScanner::CompileRules() {
-        if (!compiler) {
-            return false;
-        }
+        CHECK(compiler, false, "Compiler not created");
         
-        if (rules) {
+        if (rules != nullptr) {
             yr_rules_destroy(static_cast<YR_RULES*>(rules));
             rules = nullptr;
         }
         
-        YR_RULES* compiled_rules;
-        int result = yr_compiler_get_rules(static_cast<YR_COMPILER*>(compiler), &compiled_rules);
-        if (result == ERROR_SUCCESS) {
-            rules = compiled_rules;
-            return true;
-        }
-        return false;
+        YR_COMPILER* yr_compiler = static_cast<YR_COMPILER*>(compiler);
+        YR_RULES** ptr_yr_rules = reinterpret_cast<YR_RULES**>(&rules);
+        CHECK(yr_compiler_get_rules(yr_compiler, ptr_yr_rules) == ERROR_SUCCESS, false, "Failed to compile rules");
+        return true;
     }
 
     bool YaraScanner::LoadRules(const std::string& filePath) {
-        if (rules) {
+        if (rules != nullptr) {
             yr_rules_destroy(static_cast<YR_RULES*>(rules));
             rules = nullptr;
         }
         
-        YR_RULES* loaded_rules;
-        int result = yr_rules_load(filePath.c_str(), &loaded_rules);
-        if (result == ERROR_SUCCESS) {
-            rules = loaded_rules;
-            return true;
-        }
-        return false;
+        YR_RULES** ptr_yr_rules = reinterpret_cast<YR_RULES**>(&rules);
+        CHECK(yr_rules_load(filePath.c_str(), ptr_yr_rules) == ERROR_SUCCESS, false, "Failed to load rules from file: %s", filePath.c_str());
+        return true;
     }
 
     bool YaraScanner::SaveRules(const std::string& filePath) {
-        if (!rules) {
-            return false;
-        }
+        CHECK(rules != nullptr, false, "Rules not loaded");
         
-        int result = yr_rules_save(static_cast<YR_RULES*>(rules), filePath.c_str());
-        return result == ERROR_SUCCESS;
+        CHECK(yr_rules_save(static_cast<YR_RULES*>(rules), filePath.c_str()) == ERROR_SUCCESS, false, "Failed to save rules to file: %s", filePath.c_str());
+        return true;
     }
 
     bool YaraScanner::ScanFile(const std::string& filePath) {
-        if (!rules) {
-            return false;
-        }
+        CHECK(rules != nullptr, false, "Rules not loaded");
         
         int flags = SCAN_FLAGS_FAST_MODE // flag makes the scanning a little faster by avoiding multiple matches of the same string when not necessary
             | SCAN_FLAGS_REPORT_RULES_MATCHING; // control whether the callback is invoked for rules that are matching
 
         FILE* yara_matches_file = fopen("yara_matches.txt", "w");
-        if (!yara_matches_file) {
-            return false;
-        }
-        
+        CHECK(yara_matches_file != nullptr, false, "Failed to open file: yara_matches.txt");
+
         int result = yr_rules_scan_file(static_cast<YR_RULES*>(rules), filePath.c_str(), flags, callback_function, yara_matches_file, 0);
         fclose(yara_matches_file);
-        return result == ERROR_SUCCESS;
+        CHECK(result == ERROR_SUCCESS, false, "Failed to scan file: %s", filePath.c_str());
+        
+        return true;
     }
 
 } // namespace GView::Yara
