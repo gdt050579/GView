1d9409710e286c2f7c90758175baac061540d35f
Ana-Maria <95433627+Ana-Maria-C@users.noreply.github.com>
2026-01-06 00:12:43 +0200

    feat: implement hash computation

--- GenericPlugins/HashAnalyzer/include/HashAnalyzer.hpp
+++ GenericPlugins/HashAnalyzer/include/HashAnalyzer.hpp
@@ -9,12 +9,20 @@ using namespace AppCUI::Utils;
 using namespace AppCUI::Application;
 using namespace AppCUI::Controls;
 
-// Placeholder dialog class for future implementation
 class HashAnalyzerDialog : public Window, public Handlers::OnButtonPressedInterface
 {
   private:
     Reference<GView::Object> object;
     Reference<Button> close;
+    
+    Reference<RadioBox> computeForFile;
+    Reference<RadioBox> computeForSelection;
+    Reference<ListView> hashesList;
+    Reference<Button> computeBtn;
+
+    std::vector<TypeInterface::SelectionZone> selectedZones;
+
+    void ComputeHash();
 
   public:
     HashAnalyzerDialog(Reference<GView::Object> object);
